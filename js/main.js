6fd5776bdf54185745539422f99e7fe9ce96b1f2
bugpanye <gabazoo@gmail.com>
2013-03-18 13:16:53 +1100

    templating

--- js/main.js
+++ js/main.js
@@ -1,24 +1,15 @@
 var app = {
 
     findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
+		var self = this;
+		this.store.findByName($('.search-key').val(), function(employees) {
+			$('.employee-list').html(self.employeeLiTpl(employees));
+		});
     },
 
     initialize: function() {
-		var self = this;
-		this.store = new MemoryStore(function() {
-			self.showAlert('Store Initialized', 'Info');
-		});
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+		this.homeTpl = Handlebars.compile($("#home-tpl").html());
+		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
     },
 	
 	showAlert: function (message, title) {
@@ -27,6 +18,11 @@ var app = {
     } else {
         alert(title ? (title + ": " + message) : message);
     }
+	},
+	
+	renderHomeView: function() {
+		$('body').html(this.homeTpl());
+		$('.search-key').on('keyup', $.proxy(this.findByName, this));
 	}
 
 };
