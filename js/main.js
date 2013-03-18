22aee42fe9aeddc54f868aaf2a5243709f465997
bugpanye <gabazoo@gmail.com>
2013-03-18 11:40:54 +1100

    adding notifications

--- js/main.js
+++ js/main.js
@@ -14,9 +14,20 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+		var self = this;
+		this.store = new MemoryStore(function() {
+			self.showAlert('Store Initialized', 'Info');
+		});
+		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+	
+	showAlert: function (message, title) {
+    if (navigator.notification) {
+        navigator.notification.alert(message, null, title, 'OK');
+    } else {
+        alert(title ? (title + ": " + message) : message);
     }
+	}
 
 };
 
