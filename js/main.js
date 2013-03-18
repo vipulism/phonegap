1824e4fd6459d8db37892cbb970de7af7e3c8916
bugpanye <gabazoo@gmail.com>
2013-03-18 14:25:51 +1100

    Updating layout to become native

--- js/main.js
+++ js/main.js
@@ -1,30 +1,12 @@
 var app = {
 
-    findByName: function() {
+    initialize: function() {		
 		var self = this;
-		this.store.findByName($('.search-key').val(), function(employees) {
-			$('.employee-list').html(self.employeeLiTpl(employees));
+		this.store = new MemoryStore(function() {
+			$('body').html(new HomeView(self.store).render().el);
 		});
     },
-
-    initialize: function() {
-		this.homeTpl = Handlebars.compile($("#home-tpl").html());
-		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-    },
 	
-	showAlert: function (message, title) {
-    if (navigator.notification) {
-        navigator.notification.alert(message, null, title, 'OK');
-    } else {
-        alert(title ? (title + ": " + message) : message);
-    }
-	},
-	
-	renderHomeView: function() {
-		$('body').html(this.homeTpl());
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));
-	}
-
 };
 
 app.initialize();
